d33303ff90fc83d59ff6eff981875ed9bf1b291d
mahmoudeltohamy345 <mahmoudeltohamy345@gmail.com>
2021-07-29 06:21:37 +0200

    added Java docs

--- src/com/company/API.java
+++ src/com/company/API.java
@@ -17,6 +17,14 @@ public class API {
     }
     private String jsonString;
 
+    /**
+     * This method reads a topology from a given JSON file
+     * and store it in the memory.
+     *
+     *
+     * @param  fileName  name of given JSON file
+     * @return      the JSON string read from the file
+     */
     public String readJSON(String fileName) throws FileNotFoundException {
 
         File file = new File(fileName);
@@ -36,7 +44,12 @@ public class API {
 
 }
 
-
+    /**
+     * This method writes a given topology to a given JSON file
+     *
+     *
+     * @param  topologyID  ID of topology to be written in the JSON file
+     */
     public void writeJSON(String topologyID)  {
         if(topologyHashMap.get(topologyID)==null)
             throw new NullPointerException();
@@ -50,9 +63,16 @@ public class API {
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
 
+
+    /**
+     * This method query about which topologies
+     * are currently in the memory
+     *
+     *
+     * @return     it returns topologies that is stored in the memory
+     */
     public String queryTopologies(){
         String topologiesInMemory="";
         for (var entry : topologyHashMap.entrySet()) {
@@ -61,6 +81,15 @@ public class API {
         return topologiesInMemory;
     }
 
+
+    /**
+     * This method query about which devices are
+     * in a given topology
+     *
+     *
+     * @param  topologyID  id of topology to be queried about
+     * @return      List of devices that is in the given topology
+     */
     public List<CircuitElement> queryDevices(String topologyID){
         Topology topology=topologyHashMap.get(topologyID);
         List<CircuitElement> circuitElementList=new ArrayList<CircuitElement>();
@@ -77,6 +106,17 @@ public class API {
         return circuitElementList;
     }
 
+
+    /**
+     * This method queries about which devices is
+     * connected to a given netlist node in a given
+     * topology
+     *
+     *
+     * @param  topologyID  id of topology to be queried about
+     * @param netListNode netlistNode to search for devices connected to it
+     * @return      list of devices that is connected to the given Netlist
+     */
     public List<CircuitElement> queryDevicesWithNetlistNode(String topologyID, NetList netListNode){
         Topology topology=topologyHashMap.get(topologyID);
         List<CircuitElement> circuitElementList=queryDevices(topologyID);
@@ -91,6 +131,14 @@ public class API {
         return devicesWithNetListNode;
     }
 
+
+    /**
+     * This method deletes a given topology form
+     * the memory
+     *
+     *
+     * @param  topologyID  id of topology to be deleted
+     */
     public void deleteTopology(String topologyID){
         if(!(topologyHashMap.containsKey(topologyID)))
             throw new NullPointerException();
--- src/com/company/JsonService.java
+++ src/com/company/JsonService.java
@@ -11,6 +11,16 @@ public class JsonService  {
     private static Gson gson = new Gson();
 
 
+
+    /**
+     * This method takes a JSON string and convert it
+     * into a java classes model.
+     *
+     *
+     * @param  jsonString  JSON string to be converted
+     * @param  classOfT    class that will contain the JSON string
+     * @return  a java class that represents the JSON string
+     */
     public <T> T fromJson(String jsonString, Class<T> classOfT) {
         if(jsonString.compareToIgnoreCase("")==0)
             throw new IllegalArgumentException();
@@ -21,6 +31,15 @@ public class JsonService  {
     }
 
 
+
+    /**
+     * This method takes an object and convert it's information
+     * into JSON string.
+     *
+     *
+     * @param  object  java object to be converted into JSON string
+     * @return      JSON string that contains the object information
+     */
     public String toJson(Object object) {
         if(object==null)
             throw new NullPointerException();
